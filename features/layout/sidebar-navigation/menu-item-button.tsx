07440b4ae49ec8e10b78e15f7a12b1e745ca6a56
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-12-03 15:30:05 -0500

    Use resulting collapse icon based on isCollapse check

--- features/layout/sidebar-navigation/menu-item-button.tsx
+++ features/layout/sidebar-navigation/menu-item-button.tsx
@@ -18,11 +18,24 @@ export function MenuItemButton({
   iconSrc,
   isCollapsed,
 }: MenuItemProps) {
+  const joinedClassNamesOfIconIsCollapsed = classNames(
+    styles.icon,
+    styles.isCollapsed,
+  );
+  const resultingCollapseIconOfIsCollapsedCheck =
+    text === "Collapse" && isCollapsed
+      ? joinedClassNamesOfIconIsCollapsed
+      : styles.icon;
+
   return (
     <li className={classNames(styles.listItem, className)}>
       <Button className={styles.anchor} onClick={onClick}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img className={styles.icon} src={iconSrc} alt={`${text} icon`} />{" "}
+        <img
+          className={resultingCollapseIconOfIsCollapsedCheck}
+          src={iconSrc}
+          alt={`${text} icon`}
+        />{" "}
         {!isCollapsed && text}{" "}
       </Button>
     </li>
