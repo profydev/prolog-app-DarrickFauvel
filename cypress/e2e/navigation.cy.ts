96e7beddf0de0a45f135b310dc05c95d7b165c00
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2023-12-07 12:22:52 -0500

    Fix navigation support button (#8)
    * Change menu item component from button to link and modify propsaccordingly
    
    * Test support menu item has correct recipient email address and subject text
    
    * Modify test to account for 6 nav links
    
    ---------
    
    Co-authored-by: [Darrick Fauvel] <[darrick@darrickdevelops.com]>

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -36,11 +36,17 @@ describe("Sidebar Navigation", () => {
       cy.get("nav").contains("Collapse").click();
 
       // check that links still exist and are functionable
-      cy.get("nav").find("a").should("have.length", 5).eq(1).click();
+      cy.get("nav").find("a").should("have.length", 6).eq(1).click();
       cy.url().should("eq", "http://localhost:3000/dashboard/issues");
 
       // check that text is not rendered
       cy.get("nav").contains("Issues").should("not.exist");
+    });
+
+    it("opens the user's mail app", () => {
+      // check the mailto recipient and subject
+      cy.get('a[href*="support@prolog-app.com"]');
+      cy.get('a[href*="Support Request: "]');
     });
   });
 
@@ -80,7 +86,7 @@ describe("Sidebar Navigation", () => {
       isInViewport("nav");
 
       // check that all links are rendered
-      cy.get("nav").find("a").should("have.length", 5);
+      cy.get("nav").find("a").should("have.length", 6);
 
       // Support button should be rendered but Collapse button not
       cy.get("nav").contains("Support").should("exist");
